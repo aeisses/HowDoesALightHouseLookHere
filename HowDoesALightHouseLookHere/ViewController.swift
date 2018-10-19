14fba354a84f159204b99bb2171230344299478a
Amisha Goyal <amisha.goyal@salesforce.com>
2018-10-19 12:41:13 -0300

    Fix save logic

--- HowDoesALightHouseLookHere/ViewController.swift
+++ HowDoesALightHouseLookHere/ViewController.swift
@@ -14,6 +14,7 @@ import Network
 
 class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var saveMapButton: UIButton!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var sceneView: ARSKView!
     @IBOutlet weak var lightHouseSwitchButton: UIButton!
@@ -64,7 +65,8 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
         initLocationManager()
         self.lightHousePicker.delegate = self;
         self.lightHousePicker.dataSource = self;
-        
+        self.saveMapButton.isEnabled = false
+
         pickerData = ["PeggysCove", "capedor", "CapeGeorge", "fortpoint", "Louisbourg", "portbickerton"]
     }
     
@@ -110,6 +112,14 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
         return lighthouse
     }
 
+    func view(_ view: ARSKView, didAdd node: SKNode, for anchor: ARAnchor) {
+        if( sceneView.session.currentFrame?.worldMappingStatus == .mapped || sceneView.session.currentFrame?.worldMappingStatus == .extending) {
+            self.saveMapButton.isEnabled = true
+        } else {
+            self.saveMapButton.isEnabled = false
+        }
+    }
+
     func session(_ session: ARSession, didFailWithError error: Error) {}
     func sessionWasInterrupted(_ session: ARSession) {}
     func sessionInterruptionEnded(_ session: ARSession) {}
@@ -117,31 +127,29 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
     @IBAction func loadWorldMap(_ sender: Any) {
         networkObject.getDataFromServer { (Array, Error) in
             let info = Array?.first as! Lighthouse
-            let data = Data(base64Encoded: info.worldMap, options: [])
+            let data = Data(base64Encoded: info.worldMap, options: .ignoreUnknownCharacters)
             let worldMap = self.unarchive(worldMapData: data!)
             self.resetTrackingConfiguration(with: worldMap)
         }
     }
     
     @IBAction func saveWorldMapButton(_ sender: Any) {
-        if( sceneView.session.currentFrame?.worldMappingStatus == .mapped || sceneView.session.currentFrame?.worldMappingStatus == .extending) {
-            sceneView.session.getCurrentWorldMap { (worldMap, error) in
+        sceneView.session.getCurrentWorldMap { (worldMap, error) in
             let latitude: CLLocationDegrees = self.getCurrentCoord()?.latitude ?? 0
             let longitude: CLLocationDegrees = self.getCurrentCoord()?.longitude ?? 0
             var worldMapString: String?
             do {
                 let snapshotAnchor = SnapshotAnchor(capturing: self.sceneView)
                 worldMap?.anchors.append(snapshotAnchor!)
-            
+
                 // FIXME lots of unsafe nils here
                 let data = try NSKeyedArchiver.archivedData(withRootObject: worldMap as Any, requiringSecureCoding: true)
                 worldMapString = data.base64EncodedString()
                 let lighthouseToSave = Lighthouse(longitude: longitude as Double, latitude: latitude as Double, worldMap: worldMapString!)
                 self.networkObject.sendDataToServer(_mapObject:lighthouseToSave.toMap())
 
-                } catch {
-                    // FIXME handle this
-                }
+            } catch {
+                // FIXME handle this
             }
         }
 
